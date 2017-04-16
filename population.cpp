ece4bf5492ade6782a5b35e2c3f5f315f90b2a51
glashunti <gleibsonglas2@yahoo.com.br>
2017-04-16 00:59:59 -0300

    Exercise
    Exercise from the Book C++ Primer Plus page 112 number 5

---
+++ population.cpp
@@ -0,0 +1,26 @@
+//Exercise from the Book C++ Primer Plus page 112 number 6
+//porcent of world the population and the population from usa  
+
+#include <iostream>
+
+int main()
+{
+	long long world_population;
+	long long usa_population;
+	
+	std::cout << "Enter the world's population: " << std::endl;
+	std::cin >> world_population;
+	std::cout << "Enter the population of the US: " << std::endl;
+	std::cin >> usa_population;
+
+
+	long long percent = usa_population * 100;
+	long double usa_population_percent = percent / world_population;
+
+
+	std::cout << "The population of the Usa is " << usa_population_percent << "% of the world population." << std::endl;
+
+	return 0;
+
+
+}
